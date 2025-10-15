a1d4c542ff97327490b6bd7a6aaa70a21bba57a9
Pooja D <poojapreethi2001@gmail.com>
2025-10-15 16:05:39 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
-import "./app.css";
+import "./App.css";
 
 createRoot(document.getElementById("root")).render(<App />);
